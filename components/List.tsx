89cc47cdc24f99cb6e1c3785064c6ea0587ebe3a
rin39 <89615859+rin39@users.noreply.github.com>
2022-10-16 18:21:31 +0300

    List component quick fix

--- components/List.tsx
+++ components/List.tsx
@@ -14,12 +14,12 @@ interface ListProps {
 /**
  * List of messages, threads or boards
  */
-export default function List({ of: test }: ListProps) {
-  if (test.length === 0) return null;
-  if ((test[0] as BoardDocument).alias !== undefined) {
+export default function List({ of }: ListProps) {
+  if (of.length === 0) return null;
+  if ((of[0] as BoardDocument).alias !== undefined) {
     return (
       <ul className={styles["board-list"]}>
-        {(test as BoardDocument[]).map((board) => (
+        {(of as BoardDocument[]).map((board) => (
           <li key={board._id}>
             <Link href={`/board/${board.alias}`}>{board.name}</Link>
           </li>
@@ -27,19 +27,19 @@ export default function List({ of: test }: ListProps) {
       </ul>
     );
   }
-  if ((test[0] as ThreadDocument).name !== undefined) {
+  if ((of[0] as ThreadDocument).name !== undefined) {
     return (
       <ul className={styles["thread-list"]}>
-        {(test as ThreadDocument[]).map((thread) => (
+        {(of as ThreadDocument[]).map((thread) => (
           <ThreadListItem key={thread._id} thread={thread} />
         ))}
       </ul>
     );
   }
-  if ((test[0] as MessageDocument).text !== undefined) {
+  if ((of[0] as MessageDocument).text !== undefined) {
     return (
       <ul className={styles["message-list"]}>
-        {(test as MessageDocument[]).map((message, idx) => (
+        {(of as MessageDocument[]).map((message, idx) => (
           <MessageListItem key={message._id} message={message} idx={idx} />
         ))}
       </ul>
